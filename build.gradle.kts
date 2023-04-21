ea9ad8a9fd70f940c5fbb12316dff9bceb56c4c4
Luis Beu <me@luisbeu.de>
2023-04-21 14:19:46 +0200

    add github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,43 @@
 plugins {
-    id("fabric-loom")
-    kotlin("jvm").version(System.getProperty("kotlin_version"))
+    id("fabric-loom");
+    kotlin("jvm").version(System.getProperty("kotlin_version"));
 }
-base { archivesName.set(project.extra["archives_base_name"] as String) }
-version = project.extra["mod_version"] as String
-group = project.extra["maven_group"] as String
-repositories {}
+base { archivesName.set(project.extra["archives_base_name"] as String); }
+version = project.extra["mod_version"] as String;
+group = project.extra["maven_group"] as String;
+repositories {};
 dependencies {
-    minecraft("com.mojang", "minecraft", project.extra["minecraft_version"] as String)
-    mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2")
-    modImplementation("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String)
-    modImplementation("net.fabricmc.fabric-api", "fabric-api", project.extra["fabric_version"] as String)
-    modImplementation("net.fabricmc", "fabric-language-kotlin", project.extra["fabric_language_kotlin_version"] as String)
-    implementation("io.ktor:ktor-server-core-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-netty-jvm:2.2.4")
-    implementation("io.ktor:ktor-server-content-negotiation:2.2.4")
-    implementation("io.ktor:ktor-serialization-gson:2.2.4")
+    minecraft("com.mojang", "minecraft", project.extra["minecraft_version"] as String);
+    mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2");
+    modImplementation("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String);
+    modImplementation("net.fabricmc.fabric-api", "fabric-api", project.extra["fabric_version"] as String);
+    modImplementation("net.fabricmc", "fabric-language-kotlin", project.extra["fabric_language_kotlin_version"] as String);
+    implementation("io.ktor:ktor-server-core-jvm:2.2.4");
+    implementation("io.ktor:ktor-server-netty-jvm:2.2.4");
+    implementation("io.ktor:ktor-server-content-negotiation:2.2.4");
+    implementation("io.ktor:ktor-serialization-gson:2.2.4");
 }
 tasks {
-    val javaVersion = JavaVersion.toVersion((project.extra["java_version"] as String).toInt())
+    val javaVersion = JavaVersion.toVersion((project.extra["java_version"] as String).toInt());
     withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-        options.release.set(javaVersion.toString().toInt())
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions { jvmTarget = javaVersion.toString() } }
-    jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } }
+        options.encoding = "UTF-8";
+        sourceCompatibility = javaVersion.toString();
+        targetCompatibility = javaVersion.toString();
+        options.release.set(javaVersion.toString().toInt());
+    };
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions { jvmTarget = javaVersion.toString() } };
+    jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } };
     processResources {
-        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.extra["mod_version"] as String, "fabricloader" to project.extra["loader_version"] as String, "fabric_api" to project.extra["fabric_version"] as String, "fabric_language_kotlin" to project.extra["fabric_language_kotlin_version"] as String, "minecraft" to project.extra["minecraft_version"] as String, "java" to project.extra["java_version"] as String)) }
-        filesMatching("*.mixins.json") { expand(mutableMapOf("java" to project.extra["java_version"] as String)) }
-    }
+        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.extra["mod_version"] as String, "fabricloader" to project.extra["loader_version"] as String, "fabric_api" to project.extra["fabric_version"] as String, "fabric_language_kotlin" to project.extra["fabric_language_kotlin_version"] as String, "minecraft" to project.extra["minecraft_version"] as String, "java" to project.extra["java_version"] as String)) };
+        filesMatching("*.mixins.json") { expand(mutableMapOf("java" to project.extra["java_version"] as String)) };
+    };
     java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-        withSourcesJar()
-    }
+        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) };
+        sourceCompatibility = javaVersion;
+        targetCompatibility = javaVersion;
+        withSourcesJar();
+    };
+    exec {
+        commandLine ("echo", "##[set-output name=version;]${project.version}");
+    };
 }
